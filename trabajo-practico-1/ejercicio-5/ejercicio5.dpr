4e01786069182e5568ab5a6a96c468e7ea7cdceb
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-03-19 18:57:45 -0300

    creacion ejercicio 5

---
+++ trabajo-practico-1/ejercicio-5/ejercicio5.dpr
@@ -0,0 +1,14 @@
+program ejercicio5;
+
+uses
+  Vcl.Forms,
+  formEj5 in 'formEj5.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
