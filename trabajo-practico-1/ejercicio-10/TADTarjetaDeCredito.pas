57e168a8b408ff70f8eb0177236d8bc41bf6b18a
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2022-03-25 23:26:44 -0300

    Definición de objeto

--- trabajo-practico-1/ejercicio-10/TADTarjetaDeCredito.pas
+++ trabajo-practico-1/ejercicio-10/TADTarjetaDeCredito.pas
@@ -2,6 +2,33 @@ unit TADTarjetaDeCredito;
 
 interface
 
+type
+
+  EMISORAS = (Visa, Mastercard, AmericanExpress, Maestro);
+
+  regLimite = record
+    unaCuota: integer;
+    variasCuotas: integer;
+  end;
+
+  TarjetaDeCredito = object
+  private
+    numero: string;
+    fechaVencimiento: TDateTime;
+    limite: regLimite;
+  public
+    procedure setNumero(num: string);
+    function getNumero():string;
+    procedure setFecha(fecha: TDateTime);
+    function getFecha():TDateTime;
+    procedure setLimite(lim: regLimite);
+    function getLimite():regLimite;
+    function validarNro():boolean;
+    function entidadEmisora():EMISORAS;
+    function comprar(monto:integer):boolean;
+    function mostrarLimites():regLimite;
+  end;
+
 implementation
 
 end.
